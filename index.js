599fd197f932f776b8208c7f853838a3e50043e9
thomasmckenna <thomasmckenna12@gmail.com>
2021-07-15 14:09:03 -0400

    Thomass-MacBook-Pro:phase-0-pac-3-arithmetic-lab

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+
+}
+
+function subtract(){
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return a + b; 
+}
+
+function subtract(a, b) {
+    return a - b; 
+}
+
+function multiply(a, b) {
+    return a * b; 
+}
+
+function divide(a, b) {
+    return a / b; 
+}
+
+
+function increment(n) {
+    return n += 1; 
+}
+
+function decrement(n) {
+    return n -= 1; 
+}
+
+function makeInt(n) {
+    let number =  parseInt(n, 10); 
+    return number;  
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10) ; 
+}
+
+
